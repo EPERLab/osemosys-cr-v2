dcd0583b1c7164ac5bba654124364eb279fe5e76
Bernardo <bjza09@hotmail.com>
2021-11-19 10:12:21 -0600

    Cambios formato

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Osemosys Guatemala'
+copyright = '2021, Luis Fernando Victor'
+author = 'Luis Fernando Victor'
 
 release = '0.1'
 version = '0.1.0'
